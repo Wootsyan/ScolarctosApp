d567f37abfad8a47b8f9c12c0695e2291530abf9
Wootsyan <jakublucjan98@gmail.com>
2024-01-22 22:50:33 +0100

    Add School model

--- dashboard/models/school.py
+++ dashboard/models/school.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from users.models import CustomUser as User
 
 class School(models.Model):
     FIRST_LEVEL = 1
@@ -9,10 +11,15 @@ class School(models.Model):
         (SECOND_LEVEL, 'Ponadpodstawowa'),
     )
 
-    'name
-    'street
-    'postcode
-    'city
-    'school_type
-    'accepted
-    'added_by
+    name = models.CharField(max_length=63, unique=True)
+    street = models.CharField(max_length=63)
+    postcode = models.CharField(max_length=6, validators=[MinLengthValidator(6)])
+    city = models.CharField(max_length=63)
+    school_type = models.PositiveSmallIntegerField(choices=SCHOOL_TYPE_CHOICES, default=FIRST_LEVEL)
+    accepted = models.BooleanField(default=False)
+    added_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'Name: {self.name} | Street: {self.street} | Postcode: {self.postcode} | City: {self.city} | Type: {self.school_type} | Accepted: {self.accepted}'
+    
+    
